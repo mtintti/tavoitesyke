ba0b7276ee3314d5198220986a2ac1e586873486
mtintti <tiina.mantykivi@gmail.com>
2024-09-20 10:40:28 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
+import { useState } from 'react';
+import { Button } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState("");
+  const [lower, setLower] = useState(null);
+  const [upper, setUpper] = useState(null);
+
+  const calculate = () => {
+    const ageparsed = parseInt(age);
+      const lowerLimit = (220 - ageparsed) * 0.65;
+      const upperLimit = (220 - ageparsed) * 0.85;
+      setLower(lowerLimit.toFixed(2));
+      setUpper(upperLimit.toFixed(2));
+  
+      
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput placeholder="Enter your age"
+        onChangeText={setAge}
+        value={age} 
+        //inputMode="numeric"
+        />
+      <Text>Heart rate limits</Text>
+      <Text>{lower} - {upper}</Text>
+      <Button title="Calculate" color="blue" onPress={calculate}></Button>
       <StatusBar style="auto" />
     </View>
   );
